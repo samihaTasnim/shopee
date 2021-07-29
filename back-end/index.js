b59d2390c8d092664b93a65dc5e9b8dd37c9b7a4
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-07-29 23:16:45 +0600

    mongoose initiated and data models created

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
+import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './config/db.js';
 
 dotenv.config()
+const app = express()
+app.use(express.json())
+connectDB()
+
 const port = process.env.PORT || 5000
+console.log(`port: ${process.env.PORT}`);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Yeah server working!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
